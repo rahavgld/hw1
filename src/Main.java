7e42d0385b3242562e840e5bca9d3e172b81a427
svetlana goldschmidt <svetl.s123@gnail.com>
2022-06-23 12:20:05 +0200

    mein 1 commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+package ru.skypro;
+
+public class Main {
+
+public static void main (String()args)
+}
+Task 1
+int pig=2;
+byte cat=5;
+short cow=10;
+long bear=100L;
+flot flourWeight=1.98f;
+double saltWeight=3.5678;
+boolean pigIsAgult=pig>5;
+char d='!*';
+
+Task 2
+double boxer1=78.2;
+double boxser2=82.7;
+double weightAllboxers=boxser1+boxser2;
+dooble weightDifferenceBoxsers=boxser2-boxser1;
+System.out.println("total weight boxsers"+weightOfAllBoxsers);
+System.out.println("overal weight difference"+weitDifference);
+
+Task 3
+int banan=5;
+int bananWeight=80g;
+int milk=2;
+int milkWeight=105g;
+int eis=2;
+int eisWeight=100g;
+int eir=4;
+int eirWeight=70;
+int result(5*80g+2*105g+2*100g+4*70g);
+System.out.println("total weight"+result);
+
+Task 4
+
+
